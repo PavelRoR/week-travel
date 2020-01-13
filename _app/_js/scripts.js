ffc2ec896fc3964537a640efc270e17b885bf587
pavelp21 <pavelvp2182@yandex.ru>
2020-01-13 18:59:02 +0500

    Main page is typeseted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,10 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
-/*Конец документа*/
+    /*Конец документа*/
 });
